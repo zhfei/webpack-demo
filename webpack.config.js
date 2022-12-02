d16ef077dc58057d70fd74bae7d6468a9c857acd
zhfei <1174977961@qq.com>
2022-12-02 23:28:01 +0800

    feat: npm i webpack-dev-server@3.11.2 -D 添加webpack监听实时打包服务工具

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,9 @@ module.exports = {
     mode: "development", //mode: development, production 开发模式与生产模式配置
 
     entry: path.join(__dirname, "./src/index123.js"), //自定义webpack的打包入口
-
     output: {
         path: path.join(__dirname, "./dist"), //自定义webpack的打包导出目录
         filename: 'bundle.js' //自定义webpack的打包导出文件名
     }
-
-
 
 }

ffca17a7007eda902a6b5696d793e543b1df6764
zhfei <1174977961@qq.com>
2022-12-02 23:14:20 +0800

    modify: 自定义webpack的打包入口和 产物导出目录和文件名

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+
+
 module.exports = {
-    mode: "development" //mode: development, production 开发模式与生产模式配置
+    mode: "development", //mode: development, production 开发模式与生产模式配置
+
+    entry: path.join(__dirname, "./src/index123.js"), //自定义webpack的打包入口
+
+    output: {
+        path: path.join(__dirname, "./dist"), //自定义webpack的打包导出目录
+        filename: 'bundle.js' //自定义webpack的打包导出文件名
+    }
+
+
+
 }

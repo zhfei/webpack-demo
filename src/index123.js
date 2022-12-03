51c273a1364f18bf1b11851b6e5199f6e363e4f0
zhfei <1174977961@qq.com>
2022-12-03 14:11:42 +0800

    feat: 在webpack打包入口index.js中添加css模块

--- src/index123.js
+++ src/index123.js
@@ -1,5 +1,10 @@
 import $ from 'jquery'
 
+//webpack的打包入口是这里，所以将index.css从这里引入就可以被打包进去
+//ES6中，一切皆模块，所以使用import导入
+import './css/index.css'
+
+
 $(function () {
     $('li:odd').css('background-color','red')
     $('li:even').css('background-color','green')

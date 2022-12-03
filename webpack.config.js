012a6ff22e1d1c139c6d4c7d3b1d9732484f2101
zhfei <1174977961@qq.com>
2022-12-03 11:09:26 +0800

    add: npm i html-webpack-plugin@5.3.2 -D安装页面自动copy插件，执行webpack脚本时自动在http根目录内存中拷贝一份index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
+//导入插件，得出一个构造函数
+const HtmlPlugin = require('html-webpack-plugin')
+//创建插件实例对象
+const htmlplugin = new HtmlPlugin({
+    template: './src/index.html', //copy文件路径源
+    filename: './index.html'      //copy文件路径目标
+})
 
 module.exports = {
     mode: "development", //mode: development, production 开发模式与生产模式配置
@@ -8,6 +15,9 @@ module.exports = {
     output: {
         path: path.join(__dirname, "./dist"), //自定义webpack的打包导出目录
         filename: 'bundle.js' //自定义webpack的打包导出文件名
-    }
+    },
+    plugins: [
+        htmlplugin //添加htmlplugin插件对象，webpack在运行前，读取这个插件实例并运行
+    ]
 
 }

10c4d7ad133423261e7ff27cc65f294bd5bc5bdf
zhfei <1174977961@qq.com>
2022-12-04 10:13:35 +0800

    feat: 在webpack.config.js中添加"@"目录索引配置，在开发时推荐使用"@/images/logo.jpg"这样的方式导入模块

--- src/index123.js
+++ src/index123.js
@@ -2,11 +2,11 @@ import $ from 'jquery'
 
 //webpack的打包入口是这里，所以将index.css从这里引入就可以被打包进去
 //ES6中，一切皆模块，所以使用import导入
-import './css/index.css'
+import '@/css/index.css'
 //导入less类型模块
-import './css/index.less'
+import '@/css/index.less'
 //导入图片类型模块
-import img from './images/cat.jpeg'
+import img from '@/images/cat.jpeg'
 
 $('.box').attr('src',img)
 
--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,13 @@ module.exports = {
             //exclude排除/node_modules/目录下的第三方包
             {test: /\.js$/, use:'babel-loader', exclude: /node_modules/},
         ]
+    },
+
+    resolve: {
+        //告诉webpack,@符号表示src这一层目录， 开始时推荐使用“@/images/logo.jpj”来导入模块
+        alias: {
+            "@": path.join(__dirname, './src/')
+        }
     }
 
 }
